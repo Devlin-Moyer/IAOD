1f21236cc7f9e10b19a140c2c187422f3b296798
Devlin-Moyer <33460176+Devlin-Moyer@users.noreply.github.com>
2019-03-08 11:22:05 -0500

    Update admin.py

--- results/admin.py
+++ results/admin.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from django.contrib import admin
 
 # Register your models here.
-=======
-from django.contrib import admin
-
-# Register your models here.
->>>>>>> 59179440ead6c3e9d32b5797311ca76b48343ddf
